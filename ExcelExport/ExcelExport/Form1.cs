a19efaa85bea4a622e063196d1f2c7e5b3f77520
Domonkos Bognár <domonkos.bognar@stud.uni-corvinus.hu>
2021-10-08 10:44:39 +0200

    Function CreateExcel

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -17,6 +17,10 @@ namespace ExcelExport
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
 
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         public Form1()
         {
             InitializeComponent();
@@ -29,5 +33,27 @@ namespace ExcelExport
         {
             lakasok = context.Flats.ToList();
         }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application(); // Excel elindítása és az applikáció objektum betöltése
+                xlWB = xlApp.Workbooks.Add(Missing.Value); // Új munkafüzet
+                xlSheet = xlWB.ActiveSheet; // Új munkalap
+                xlApp.Visible = true; // Control átadása a felhasználónak
+                xlApp.UserControl = true; 
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string hiba = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(hiba, "Error");
+             
+                xlWB.Close(false, Type.Missing, Type.Missing); // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }       
     }
 }
