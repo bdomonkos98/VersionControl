74c7b6cf9db339e6957b78acc33df1b4f8de4827
Domonkos Bognár <domonkos.bognar@stud.uni-corvinus.hu>
2021-10-08 10:29:22 +0200

    Add Excel Namespace

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ExcelExport
 {
@@ -20,6 +22,7 @@ namespace ExcelExport
             InitializeComponent();
 
             LoadData();
+         
         }
 
         public void LoadData()
