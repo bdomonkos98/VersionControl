0713eb4872a6aadff69039acf3cd61abb2ad10f6
Domonkos Bognár <domonkos.bognar@stud.uni-corvinus.hu>
2021-10-29 20:38:09 +0200

    Create LoadXml

--- WebService/WebService/Form1.cs
+++ WebService/WebService/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using WebService.MnbServiceReference;
 
 namespace WebService
@@ -19,8 +20,32 @@ namespace WebService
         public Form1()
         {
             InitializeComponent();
-            Consume();
+            string xmlstring = Consume();
+            LoadXml(xmlstring);
 
+            dataGridView1.DataSource = Rates;
+        }
+
+        private void LoadXml(string input)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(input);
+       
+            foreach (XmlElement item in xml.DocumentElement)
+            {           
+                RateData r = new RateData();           
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+                
+                XmlElement child = (XmlElement)item.FirstChild;
+                r.Currency = child.GetAttribute("curr");
+                r.Value = decimal.Parse(child.InnerText);
+                int unit = int.Parse(child.GetAttribute("unit"));
+                
+                if (unit != 0)
+                    r.Value = r.Value / unit;
+
+                Rates.Add(r);
+            }
         }
 
         private string Consume()
