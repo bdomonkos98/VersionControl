d1637e34b41767b1fcde5c4b6b5ac12b96185539
Domonkos Bognár <domonkos.bognar@stud.uni-corvinus.hu>
2021-10-29 20:46:54 +0200

    Creating Chart

--- WebService/WebService/Form1.cs
+++ WebService/WebService/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using WebService.MnbServiceReference;
 
@@ -20,10 +21,31 @@ namespace WebService
         public Form1()
         {
             InitializeComponent();
+
             string xmlstring = Consume();
             LoadXml(xmlstring);
+            Charting();
 
             dataGridView1.DataSource = Rates;
+        }
+
+        private void Charting()
+        {
+            chartRateData.DataSource = Rates;
+
+            Series series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         private void LoadXml(string input)
