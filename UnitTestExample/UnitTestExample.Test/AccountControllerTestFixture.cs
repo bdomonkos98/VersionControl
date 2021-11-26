143c53626b0784c21085a4df77b4e9640e13a201
Domonkos Bognár <domonkos.bognar@stud.uni-corvinus.hu>
2021-11-26 16:44:56 +0100

    Password validation

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -10,11 +10,12 @@ namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test,
-         TestCase("abcd1234", false),
-         TestCase("irf@uni-corvinus", false),
-         TestCase("irf.uni-corvinus.hu", false),
-         TestCase("irf@uni-corvinus.hu", true)
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
         ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
@@ -25,8 +26,28 @@ namespace UnitTestExample.Test
             var actualResult = accountController.ValidateEmail(email);
 
             // Assert
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual(actualResult, expectedResult);
         }
 
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcdABCD", false),
+            TestCase("abCD12", false),
+            TestCase("abCD1234", true),
+            TestCase("abCDEF1234", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(actualResult, expectedResult);
+        }
     }
 }
