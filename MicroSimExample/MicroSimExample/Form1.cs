7226b0c56e1f9d3a5992e51beee4419b97497348
Domonkos Bognár <domonkos.bognar@stud.uni-corvinus.hu>
2021-12-03 10:39:00 +0100

    Adatok beolvasása

--- MicroSimExample/MicroSimExample/Form1.cs
+++ MicroSimExample/MicroSimExample/Form1.cs
@@ -22,7 +22,9 @@ namespace MicroSimExample
         {
             InitializeComponent();
 
-            Population = GetPopulation(@"C:\Temp\nép-teszt.csv");
+            Population = GetPopulation(@"C:\Temp\nép.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
         }
 
         public List<Person> GetPopulation(string csvpath)
@@ -42,8 +44,49 @@ namespace MicroSimExample
                     });
                 }
             }
-
             return population;
+        }
+
+        public List<BirthProbability> GetBirthProbabilities(string csvpath)
+        {
+            List<BirthProbability> birthProbabilities = new List<BirthProbability>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    birthProbabilities.Add(new BirthProbability()
+                    {
+                        Age = int.Parse(line[0]),                      
+                        NbrOfChildren = int.Parse(line[1]),
+                        BirthP = 
+                    });
+                }
+            }
+
+            return birthProbabilities;
+        }
+
+        public List<DeathProbability> GetDeathProbabilities(string csvpath)
+        {
+            List<DeathProbability> deathProbabilities = new List<DeathProbability>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    deathProbabilities.Add(new DeathProbability()
+                    {
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+
+            return deathProbabilities;
         }
     }
 }
